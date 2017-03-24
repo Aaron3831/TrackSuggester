59e4d02bb5de3fe0dc627b9377ae3de44ed6d787
Aaron Nguyen <aaron3831@gmail.com>
2017-03-24 09:20:44 -0700

    add variables for radio options in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-  $("form#Suggester").submit(function() {
+  $("form#Suggester").submit(function(event) {
   event.preventDefault();
-    
+    var option1 = $("input#optionsRadios1").val();
+    var option2 = $("input#optionsRadios2").val();
+    var option3 = $("input#optionsRadios3").val();
+    var option4 = $("input#optionsRadios4").val();
+    var option5 = $("input#optionsRadios5").val();
+    var option6 = $("input#optionsRadios6").val();
+    var option7 = $("input#optionsRadios7").val();
+    var option8 = $("input#optionsRadios8").val();
+
+    if
   });
 });

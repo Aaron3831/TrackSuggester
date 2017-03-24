d114b69c1c703296a285bf3eaf18739c2d7bbbaf
Aaron Nguyen <aaron3831@gmail.com>
2017-03-24 10:53:47 -0700

    add css font-style, text-decoration and font-size adjustment to radio descriptions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("form#Suggester").submit(function(event) {
+    var name = $("#nameInput").val();
     var group1 = $("input:radio[name=group1]:checked").val();
     var group2 = $("input:radio[name=group2]:checked").val();
     var group3 = $("input:radio[name=group3]:checked").val();
@@ -21,8 +22,15 @@ $(document).ready(function(){
     };
     if (group1 === 'option2') {
       if (group2 === 'option3' || 'option4') {
+        if (group3 === 'option5') {
+          alert("Java might be what you're looking for")
+        }
+      }
+    };
+    if (group1 === 'option2') {
+      if (group2 === 'option3' || 'option4') {
         if (group3 === 'option6') {
-          alert("Ruby might be what you're looking for")
+          alert("C# sounds good")
         }
       }
     };

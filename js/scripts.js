271768b287dd0ab7165cd16195c39253dcac5221
Aaron Nguyen <aaron3831@gmail.com>
2017-03-24 10:25:02 -0700

    add branching for radio options

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,27 @@ $(document).ready(function(){
     var group3 = $("input:radio[name=group3]:checked").val();
     console.log();
 
-    // if (group1 ) {
-    //
-    // } else {
-    //   alert(C# might be a place to start!)
-    // }
+    if (group1 === 'option1') {
+      if (group2 === 'option3' || 'option4') {
+        if (group3 === 'option5') {
+          alert('PHP sounds good')
+        }
+      }
+    };
+    if (group1 === 'option1') {
+      if (group2 === 'option3' || 'option4') {
+        if (group3 === 'option6') {
+          alert("Ruby might be what you're looking for")
+        }
+      }
+    };
+    if (group1 === 'option2') {
+      if (group2 === 'option3' || 'option4') {
+        if (group3 === 'option6') {
+          alert("Ruby might be what you're looking for")
+        }
+      }
+    };
     event.preventDefault();
   });
 });

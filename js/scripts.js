98f772a184792b430a0e3cd8ab4fde1f3990663a
Aaron Nguyen <aaron3831@gmail.com>
2017-03-24 11:08:15 -0700

    add concatenated message in alert that includes name input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("form#Suggester").submit(function(event) {
-    var name = $("#nameInput").val();
+    var name = $("input#nameInput").val();
     var group1 = $("input:radio[name=group1]:checked").val();
     var group2 = $("input:radio[name=group2]:checked").val();
     var group3 = $("input:radio[name=group3]:checked").val();
@@ -9,28 +9,28 @@ $(document).ready(function(){
     if (group1 === 'option1') {
       if (group2 === 'option3' || 'option4') {
         if (group3 === 'option5') {
-          alert('PHP sounds good')
+          alert('Hey ' + name + ' PHP sounds good!')
         }
       }
     };
     if (group1 === 'option1') {
       if (group2 === 'option3' || 'option4') {
         if (group3 === 'option6') {
-          alert("Ruby might be what you're looking for")
+          alert('Hey ' + name +  " Ruby might be what you're looking for!")
         }
       }
     };
     if (group1 === 'option2') {
       if (group2 === 'option3' || 'option4') {
         if (group3 === 'option5') {
-          alert("Java might be what you're looking for")
+          alert('Hey ' + name +  " Java might be what you're looking for!")
         }
       }
     };
     if (group1 === 'option2') {
       if (group2 === 'option3' || 'option4') {
         if (group3 === 'option6') {
-          alert("C# sounds good")
+          alert('Hey ' + name +  " C# sounds good!")
         }
       }
     };
